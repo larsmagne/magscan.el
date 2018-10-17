1254a6201c50f531454a982a5992b98008e9eaa7
Lars Ingebrigtsen <larsi@gnus.org>
2018-10-17 17:58:59 +0200

    Scaffolding

---
+++ magscan.el
@@ -0,0 +1,28 @@
+;;; magscan.el --- Scan Magazines -*- lexical-binding: t -*-
+;; Copyright (C) 2018 Lars Magne Ingebrigtsen
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: wordpress
+
+;; magscan is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; magscan is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;;; Commentary:
+
+;; Install
+
+
+;;; Code:
+
+(require 'cl)
+
+(provide 'magscan)
+
+;;; magscan.el ends here
